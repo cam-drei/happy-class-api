5a7687d45b5d47c6e57100b52fae3fccc319ca29
Vinh Luu <vinhlt3@gmail.com>
2024-02-29 09:02:21 +0700

    delete belongs_to user in Course models

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,7 +1,6 @@
 class Course < ApplicationRecord
   validates :name, presence: true
 
-  belongs_to :user
   has_many :enroll_courses
   has_many :lessons
   has_many :subjects
--- app/models/enroll_lesson.rb
+++
@@ -1,4 +0,0 @@
-class EnrollLesson < ApplicationRecord
-  belongs_to :user
-  belongs_to :lesson
-end
