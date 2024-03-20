105e41981baed92c755fe6d4a41a6e33779a5c85
Vinh Luu <vinhlt3@gmail.com>
2024-03-20 14:13:10 +0700

    RenameDoneColumnInSubjects

---
+++ db/migrate/20240320070402_rename_done_column_in_subjects.rb
@@ -0,0 +1,5 @@
+class RenameDoneColumnInSubjects < ActiveRecord::Migration[7.1]
+  def change
+    rename_column :subjects, :done, :selected
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_03_12_081410) do
+ActiveRecord::Schema[7.1].define(version: 2024_03_20_070402) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -64,7 +64,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_03_12_081410) do
   create_table "subjects", force: :cascade do |t|
     t.string "name"
     t.text "description"
-    t.boolean "done", default: false
+    t.boolean "selected", default: false
     t.bigint "course_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
