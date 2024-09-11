dd3651575d3439fb2809158f8723076db22eaf0c
Vinh Luu <vinhlt3@gmail.com>
2024-09-11 16:07:56 +0700

    Fixed bug when login with another user in case done for subject lessons

--- app/controllers/api/v1/user_subject_lessons_controller.rb
+++ app/controllers/api/v1/user_subject_lessons_controller.rb
@@ -1,39 +1,19 @@
 class Api::V1::UserSubjectLessonsController < ApplicationController
   before_action :authenticate_user!
-  before_action :find_user_subject_lesson, only: [:mark_user_subject_lesson_as_done, :unmark_user_subject_lesson_as_done, :user_subject_lesson_contents]
+  before_action :find_user_subject_lesson, only: [:user_subject_lesson_contents]
   before_action :find_lesson, only: [:user_subject_lessons_for_lesson]
 
   def mark_user_subject_lesson_as_done
-    user_subject_lesson = current_user.user_subject_lessons.find_by(subject_lesson_id: params[:subject_lesson_id])
-  
-    if user_subject_lesson
-      user_subject_lesson.update(done: true)
-      render json: { message: 'User Subject lesson marked as done successfully' }, status: :ok
-    else
-      render json: { error: 'User Subject lesson not found' }, status: :not_found
-    end
+    update_user_subject_lesson_status(true, 'marked as done successfully')
   end
 
   def unmark_user_subject_lesson_as_done
-    user_subject_lesson = current_user.user_subject_lessons.find_by(subject_lesson_id: params[:subject_lesson_id])
-  
-    if user_subject_lesson
-      user_subject_lesson.update(done: false)
-      render json: { message: 'User Subject lesson unmarked as not done successfully' }, status: :ok
-    else
-      render json: { error: 'Failed to mark user subject lesson as not done' }, status: :not_found
-    end
+    update_user_subject_lesson_status(false, 'unmarked as done successfully')
   end
 
   def user_subject_lessons_for_lesson
     subject_lessons = @lesson.subject_lessons
     user_subject_lessons = current_user.user_subject_lessons.where(subject_lesson: subject_lessons)
-    
-    Rails.logger.debug { "Lesson: #{@lesson.id}" }
-    Rails.logger.debug { "Subject Lessons: #{subject_lessons.to_json}" }
-    Rails.logger.debug { "User Subject Lessons: #{user_subject_lessons.to_json}" }
-    Rails.logger.debug { "Current User: #{current_user.id}" }
-    Rails.logger.debug { "Current User's Subject Lessons: #{current_user.user_subject_lessons.to_json}" }
   
     render json: {
       subject_lessons: subject_lessons.as_json(include: :subject),
@@ -52,6 +32,20 @@ class Api::V1::UserSubjectLessonsController < ApplicationController
 
   private
 
+  def update_user_subject_lesson_status(status, message)
+    user_subject_lesson = current_user.user_subject_lessons.find_or_create_by(subject_lesson_id: params[:subject_lesson_id]) do |lesson|
+      lesson.done = !status
+    end
+
+    if user_subject_lesson
+      user_subject_lesson.update(done: status)
+      render json: { message: "User Subject lesson #{message}" }, status: :ok
+    else
+      Rails.logger.error "Failed to create or find User Subject lesson for subject_lesson_id: #{params[:subject_lesson_id]}"
+      render json: { error: 'User Subject lesson not found or failed to create' }, status: :not_found
+    end
+  end
+
   def find_user_subject_lesson
     @user_subject_lesson = current_user.user_subject_lessons.find_by(subject_lesson_id: params[:subject_lesson_id])
     render json: { error: 'Subject lesson not found' }, status: :not_found unless @user_subject_lesson
@@ -63,12 +57,4 @@ class Api::V1::UserSubjectLessonsController < ApplicationController
   rescue ActiveRecord::RecordNotFound
     render json: { error: 'Lesson not found' }, status: :not_found
   end
-
-  # private
-
-  # def find_user_subject_lesson
-  #   @user_subject_lesson = current_user.user_subject_lessons.find_or_initialize_by(subject_lesson_id: params[:subject_lesson_id])
-  # rescue ActiveRecord::RecordNotFound
-  #   render json: { error: 'Subject lesson not found' }, status: :not_found
-  # end
 end
