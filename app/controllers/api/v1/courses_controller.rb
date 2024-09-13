ccffb0eb9e512f08fc87c8af3a3f412bed4dbdd5
Vinh Luu <vinhlt3@gmail.com>
2024-09-13 10:54:29 +0700

    Display selected_user_subjects_for_course

--- app/controllers/api/v1/courses_controller.rb
+++ app/controllers/api/v1/courses_controller.rb
@@ -10,17 +10,28 @@ class Api::V1::CoursesController < ApplicationController
   end
 
   def lessons_for_course
-    lessons = @course.lessons.includes(:contents, subject_lessons: [:subject, :user_subject_lessons])
+    course_id = params[:course_id]
   
+    selected_user_subjects = current_user.selected_user_subjects_for_course(course_id)
+    selected_subject_ids = selected_user_subjects.pluck(:subject_id)
+  
+    lessons = @course.lessons.includes(:contents, subject_lessons: [:subject, :user_subject_lessons])
+    
+    lessons = lessons.select do |lesson|
+      lesson.subject_lessons.any? { |subject_lesson| selected_subject_ids.include?(subject_lesson.subject_id) }
+    end
+    
     lessons_details = lessons.map do |lesson|
       {
         id: lesson.id,
         name: lesson.name,
         done: lesson.subject_lessons.any? { |sl| current_user.user_subject_lessons.exists?(subject_lesson: sl, done: true) },
         contents: lesson.contents.as_json(only: [:id, :video_link, :document_link]),
-        subject_lessons: lesson.subject_lessons.map do |subject_lesson|
+        subject_lessons: lesson.subject_lessons.select do |subject_lesson|
+          selected_subject_ids.include?(subject_lesson.subject_id)
+        end.map do |subject_lesson|
           user_subject_lesson = current_user.user_subject_lessons.find_by(subject_lesson: subject_lesson)
-  
+      
           {
             id: subject_lesson.id,
             done: user_subject_lesson&.done || false,
@@ -32,6 +43,8 @@ class Api::V1::CoursesController < ApplicationController
     end
   
     render json: lessons_details, status: :ok
+  rescue ActiveRecord::RecordNotFound
+    render json: { error: 'Course not found' }, status: :not_found
   end
 
   def contents_for_course
